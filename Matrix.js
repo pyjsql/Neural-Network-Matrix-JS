e4df8b8a62b954cf78141cb2675836af2b494735
Hakan Eroğlu <hakaneroglu@gmail.com>
2019-07-27 18:24:12 +0300

    minor changes
    copy, add functions

--- Matrix.js
+++ Matrix.js
@@ -1,10 +1,66 @@
 class Matrix{
     constructor(rows,cols){
+        //Create matrix
         this.rows = rows;
         this.cols = cols;
         this.matrix = new Array(rows);
         for(let i=0; i < rows;i++){
             this.matrix[i] = new Array(cols);
+            for(let j=0; j < this.cols; j++){
+                this.matrix[i][j] = Math.random();
+            }
+        }
+    }
+    //Returns copy of matrix
+    copy(){
+        let n = new Matrix(this.rows, this.cols);
+        for(let i=0; i < this.rows; i++){
+            for(let j=0; j < this.cols; j++){
+                n.matrix[i][j] = this.matrix[i][j];
+            }
+        }
+        return n;
+    }
+    //prints table of matrix in js console
+    print(){
+        console.table(this.matrix);
+    }
+    // add a number or another matrix
+    add(m){
+        if(m instanceof Matrix){
+            if(this.cols === m.cols && this.rows === m.rows){
+                for(let i=0; i < this.rows; i++){
+                    for(let j=0; j < this.cols; j++){
+                        this.matrix[i][j] += m.matrix[i][j];
+                    }
+                }
+            }else{
+                throw "Matrix col and row numbers not equal!";
+            }
+        }else{
+            for(let i=0; i < this.rows; i++){
+                for(let j=0; j < this.cols; j++){
+                    this.matrix[i][j] += m;
+                }
+            }
+        }
+    }
+    // returns new matrix
+    static add(m1,m2){
+        if(m1 instanceof Matrix && m2 instanceof Matrix){
+            let n = m1.copy();
+            n.add(m2);
+            return n;
+        }else{
+            throw "Variable must be Matrix!";
         }
     }
 }
+
+let a = new Matrix(3,2);
+let b = new Matrix(3,2);
+let c = Matrix.add(a,b);
+a.print();
+b.print();
+c.print();
+
--- sketch.js
+++ sketch.js
@@ -3,11 +3,6 @@ function setup() {
   // put setup code here
   createCanvas(640,480);
   
-  let nn = new NeuralNetwork(24,16,4);
-  let a = new Matrix(24,1);
-  let input = a.toArray();
-  let output = nn.feedforward(input);
-  output.print();
 }
 
 function draw() {
